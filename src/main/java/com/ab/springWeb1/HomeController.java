1b853cb75de2277eb53694b9f6e26187c8b92a77
bitturana-ab <b2ra830@gmail.com>
2025-07-17 15:23:03 +0530

    RestController and send response body to the client

--- src/main/java/com/ab/springWeb1/HomeController.java
+++ src/main/java/com/ab/springWeb1/HomeController.java
@@ -2,11 +2,14 @@ package com.ab.springWeb1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @RequestMapping("/")
+    @ResponseBody
     public String greet(){
         System.out.println("I am here.");
         return "Welcome to ab!!";
