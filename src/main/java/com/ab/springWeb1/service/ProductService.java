eacff68deb03d326333780685d782929dcb4b628
AB <b2ra830@gmail.com>
2025-08-30 13:24:27 +0530

    search product through url have done

--- src/main/java/com/ab/springWeb1/service/ProductService.java
+++ src/main/java/com/ab/springWeb1/service/ProductService.java
@@ -12,7 +12,9 @@ public class ProductService {
 //    create dummy data coll, but we should use DB for data
     List<Product> products = Arrays.asList(
             new Product(01,"samsung a54",34000),
-            new Product(02,"samsung a55",45000));
+            new Product(02,"samsung a55",45000),
+            new Product(03,"samsung a53",42000)
+);
 
     public List<Product> getProducts(){
         return products;
@@ -20,7 +22,16 @@ public class ProductService {
 
 
     public Product getProductById(int prodId) {
-        return products.getFirst();
+//        or use stream api for filter but now im using for loop
+//        for(int i = 0;i<products.toArray().length;i++){
+//            if(products.get(i).getProdId() == prodId){
+//                return products.get(i);
+//            }
+//        };
+        for(Product prod : products){
+            if(prod.getProdId() == prodId) return prod;
+        }
+        return new Product(0,"not found",00);
     }
 }
 
